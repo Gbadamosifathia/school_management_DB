4f38b02eca277a38882310f349e1479e94ed3a33
Gbadamosifathia <gbadamosifathia02@gmail.com>
2026-04-06 09:35:22 +0100

    added task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,2 @@
+INSERT INTO students (student_id, student_name, email, date_of_birth, address) 
+VALUES(1, 'Gbadamosi Fathia', 'gbadamosifathia02@gmail.com', '2005-05-08', 'No 5, Oke Aro Street, Lagos')
