7ab604f1eb129756fd2cc9b29e9b4fa530f3a023
Gbadamosifathia <gbadamosifathia02@gmail.com>
2026-04-06 09:37:23 +0100

    added task_7.sql

---
+++ task_7.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (student_id, student_name, email, date_of_birth, address)
+VALUES(2, 'Amina Hassan', 'ahassan@school.com', '2001-03-15', '2, Learning Street'),
+(3, 'Chidi Okafor', 'cokafor@school.com', '1999-07-20', '3, Learning Street'),
+(4, 'Ngozi Adeyemi', 'nadeyemi@school.com', '2002-11-05', '4, Learning Street');
